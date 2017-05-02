e8426f5761d51816b146e226f844b2cf69e2c786
Simone Pandolfi <simopandolfi@gmail.com>
2017-05-02 22:34:55 +0200

    Included message load support

--- builders/python_django/templates/models.py
+++ builders/python_django/templates/models.py
@@ -78,39 +78,73 @@ class {{ message.fullname|join('__') }}:
                 {% set value = [] %}
             {% endif %}
             {% set default_value = None %}
-            {% if field.multiplicity == 'repeated' %}
-                {% set initial_value = [] %}
-            {% elif field.multiplicity == 'optional' %}
-                {% if 'default' in field.modifiers %}
-                    {% set default_value = field.modifiers['default'] %}
-                {% else %}
-                    {% set default_value = (field.data_type | core.map_default_value)() %}
+            {% if field.data_type is not mapping %}
+                {% if field.multiplicity == 'repeated' %}
+                    {% set initial_value = [] %}
+                {% elif field.multiplicity == 'optional' %}
+                    {% if 'default' in field.modifiers %}
+                        {% set default_value = field.modifiers['default'] %}
+                    {% else %}
+                        {% set default_value = (field.data_type|core.map_default_value)() %}
+                    {% endif %}
                 {% endif %}
             {% endif %}
             {% set required = False %}
             {% if field.multiplicity == 'required' %}
                 {% set required = True %}
             {% endif %}
-        self.__{{ field.name }} = {'initialized': False, 'value': {{ value }}, 'default_value': {{ default_value }}, 'required': {{ required }}}
+            {% set type = field['data_type'] %}
+            {% if type is mapping %}
+                {% set type = type.fullname|join('__') %}
+            {% endif %}
+        self.__{{ field.name }} = {'initialized': False, 'value': {{ value }}, 'default_value': {{ default_value }}, 'required': {{ required }}, 'type': {{ type }}}
         {% endfor %}
 
-    {% for field in message.fields.values() if field.multiplicity != 'repeated' %}
+    {% for field in message.fields.values() %}
     @property
     def {{field.name}}(self):
         if not self.__{{ field.name }}['required'] and not self.__{{ field.name }}['initialized']:
             return self.__{{ field.name }}['default_value']
         return self.__{{ field.name }}['value']
 
+    {%  if field.multiplicity != 'repeated' %}
     @{{ field.name }}.setter
     def {{ field.name }}(self, value):
         if not isinstance(value, {{ field.data_type|python.map_data_type }}):
             raise TypeError("Expected '{{ field.name }}' is a {{ field.data_type|python.map_data_type }}, got: {0}".format(type(value).__name__))
         self.__{{ field.name }}['value'] = value
         self.__{{ field.name }}['initialized'] = True
+    {% else %}
+    @{{field.name}}.setter
+    def {{ field.name }}(self, value):
+        if not hasattr(value, '__iter__') or not callable(value.__iter__):
+            raise Exception("Value must be an instance of iterable, got: {0}".format(type(value).__name__))
+        for arg in value:
+            if not isinstance(arg, {{ field.data_type|python.map_data_type }}):
+                raise TypeError("Expected '{{ field.name }}' is a {{ field.data_type|python.map_data_type }}, got: {0}".format(type(arg).__name__))
+        self.__{{ field.name }}['value'] = []
+        self.add_{{ field.name }}(*value)
+
+    def add_{{ field.name }}(self, *args):
+        for arg in args:
+            if not isinstance(arg, {{ field.data_type|python.map_data_type }}):
+                raise TypeError("Expected '{{ field.name }}' is a {{ field.data_type|python.map_data_type }}, got: {0}".format(type(arg).__name__))
+        self.__{{ field.name }}['value'] += list(args)
+        self.__{{ field.name }}['initialized'] = True
+
+    def del_{{ field.name }}(self, *args):
+        """ Removes all occurrences of the items. """
+        for arg in args:
+            try:
+                while True:
+                    self.__{{ field.name }}['value'].remove(arg)
+            except ValueError:
+                pass
+    {% endif %}
 
     {% endfor %}
     {% for modelname, fieldlist in (message|core.map_message_field_to_model).items() %}
-    def load_model_{{ modelname|replace('.', '__') }}(self, model):
+    def put_{{ modelname|replace('.', '__') }}(self, model):
         if not isinstance(model, {{ modelname|replace('.', '__') }}):
             raise TypeError("Model must be an instance of {{ modelname|replace('.', '__') }}, got: {0}".format(type(model).__name__))
         {% for field in fieldlist %}
@@ -122,16 +156,41 @@ class {{ message.fullname|join('__') }}:
         {% endfor %}
 
     {% endfor %}
-    def serialize(self):
+    def dump(self):
         {% for field in message.fields.values() if field.multiplicity == 'required' %}
         if not self.__{{ field.name }}['initialized']:
             raise Exception("Could not serialize '{{ message.fullname|join('.') }}', '{{ field.name }}' has not been initialized!")
         {% endfor %}
         return json.dumps({
             {% for field in message.fields.values() %}
+                {% if field.data_type is mapping %}
+            '{{ field.name }}': self.{{ field.name }}.dump(),
+                {% else %}
             '{{ field.name }}': self.{{ field.name }},
+                {% endif %}
             {% endfor %}
         })
+
+    @classmethod
+    def load(cls, data):
+        obj = json.loads(data)
+        new_instance = cls()
+        {% for field in message.fields.values() %}
+            {% if field.data_type is mapping %}
+                {% if field.multiplicity == 'repeated'%}
+        new_instance.{{ field.name }} = [{{ field.data_type|python.map_data_type }}.load(item) for item in obj['{{ field.name }}']]
+                {% else %}
+        new_instance.{{ field.name }} = {{ field.data_type|python.map_data_type }}.load(obj['{{ field.name }}'])
+                {% endif %}
+            {% else %}
+        new_instance.{{ field.name }} = obj['{{ field.name }}']
+            {% endif %}
+        {% endfor %}
+
+        {% for _message in message|core.nested_messages %}
+
+{{ make_message(_message) }}
+        {%- endfor %}
 {% endmacro %}
 
 
